c1cfe2e1520fa7ba74bd94c17cc5910ef9e2c9bf
Juan Carlos Guillen Silva <TuCorreoElectronico@ejemplo.com>
2024-05-04 20:43:01 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+      <h1>PI. Aplicación con tu entorno de desarrollo, Framework</h1>
+      <FilterByStatus/>
+      <FilterByName/>
+      <ListCharacters />
+  </div>
+</template>
+
+<script>
+import ListCharacters from './components/ListCharacters';
+import FilterByStatus from './components/FilterByStatus';
+import FilterByName from './components/FilterByName';
+export default {
+  name: 'App',
+  components: {
+    ListCharacters,
+    FilterByStatus,
+    FilterByName
+}
+
+
+
+}
+</script>
+
+<style lang="scss">
+:root{
+  --background-body:#ffffff;
+  --background-card:#00a993;
+  --text-wihite:#fff;
+  --text-gray:#c0c0c0;
+  --text-orange:#ff0000;
+}
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body{
+  background-color: var(--background-body);
+  color: var(--text-wihite);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+h1{
+  margin-bottom: 3rem;
+  text-align: center;
+}
+.container{
+  width: 900px;
+  max-width: 100%;
+  margin: 5rem auto;
+}
+img{
+  width: 100%;
+}
+</style>
---
+++ src/components/FilterByStatus.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="filter">
+    <div class="item" @click="filter('')">
+        PERSONAJES
+    </div>
+    <div class="item" @click="filter('Alive')">
+        VIVOS
+    </div>
+    <div class="item" @click="filter('Dead')">
+        MUERTOS
+    </div>
+    <div class="item" @click="filter('unknown')">
+        DESCONOCIDOS
+    </div>
+  </div>
+</template>
+
+<script >
+import { useStore } from 'vuex';
+
+export default{
+    setup(){
+        const store=useStore()
+        const filter=((status)=>{
+            store.dispatch('filterByStatus',status)
+        })
+        return{
+            filter
+        }
+    }
+}
+
+</script>
+
+<style lang="scss">
+.filter{
+    width: 600px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(4,1fr);
+    border-radius: 8px;
+    overflow: hidden;
+    .item{
+        padding: 1rem 0.5rem;
+        background-color: var(--background-card);
+        text-align: center;
+        cursor: pointer;
+        &:hover{
+            color: var(--text-orange);
+        }   
+    }
+}
+</style>
